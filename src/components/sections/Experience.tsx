29d8c8c115ead49a89ae96ce7800b6db3f95d276
JAYABHARGAVI B <90489745+jayaatgh@users.noreply.github.com>
2026-05-24 08:45:20 +0530

    Update Experience.tsx

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -5,25 +5,28 @@ const experiences = [
         title: "Programmer Analyst | Data Enginee",
         company: "Cognizant",
         period: "Nov 2024 — Present",
-        description:
-            "The Enterprise Data Engine - Healthcare"
-            "I work on distributed backend and data processing systems, focusing on scalability, fault tolerance, and system reliability.",
+        description: [
+            "The Enterprise Data Engine - Healthcare",
+            "I work on distributed backend and data processing systems, focusing on scalability, fault tolerance, and system reliability."
+        ],
     },
     {
         title: "Founder & Full-Stack Developer",
         company: "Castororo",
         period: "2024 - 2025",
-        description:
-            "The Innovative Builder"
-            "I design and build clean, friendly websites for individuals and small teams — from structure and layout to development and deployment.",
+        description: [
+            "The Innovative Builder",
+            "I design and build clean, friendly websites for individuals and small teams — from structure and layout to development and deployment."
+        ],
     },
     {
         title: "Python Developer Intern",
         company: "Shiash Info Solutions Pvt. Ltd.",
         period: "Oct 2022 — Dec 2022",
-        description:
-            "The Backend Foundation"
-            "Built scalable RESTful APIs using Django and PostgreSQL to efficiently handle high-throughput data requests. Enhanced overall API performance and query efficiency by 25% through advanced indexing and data retrieval strategies.",
+        description: [
+            "The Backend Foundation",
+            "Built scalable RESTful APIs using Django and PostgreSQL to efficiently handle high-throughput data requests. Enhanced overall API performance and query efficiency by 25% through advanced indexing and data retrieval strategies."
+        ],
     }
 ];
 
